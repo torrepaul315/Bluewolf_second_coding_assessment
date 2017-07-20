77d5c71ab1b6cbedd7d70f762e2110de2c8025bb
Paul Torres <torrepaul@gmail.com>
2017-07-20 11:39:22 -0600

    got chartjs hooked up, no functionality built yet, moving to setting up a backend

--- second_coding_assignment/public/javascripts/modules/app.module.js
+++ second_coding_assignment/public/javascripts/modules/app.module.js
@@ -1,6 +1,9 @@
 (function () {
   'use strict';
 // may need to change from moment to angular moment (as per tom! )
-   angular.module('app', ['ui.router'])
+   angular.module('app', ['ui.router', 'chart.js'])
   // angular.module('app', ['ui.router'])
 }());
+
+
+//, 'chart.js','angular-chart'
