b7bdc903af00fe8372fbe300d81d25dc285a496b
Paul Torres <torrepaul@gmail.com>
2017-10-26 18:12:04 -0600

    tweaked users

--- seeds/01_user.js
+++ seeds/01_user.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('weather_user').insert([
         {user_name: 'Marc Benioff', email:'thebigcheese@salesforce.com'},
-        {user_name: 'Eric Berridge',email:'thebigenchilada@bluewolf.com'},
+        {user_name: 'Gireesh Sonnad',email:'thebigenchilada@silverlinecrm.com'},
         {user_name: 'Codey the Bear',email:'kindofastar@trailhead.com'},
       ]);
     });
